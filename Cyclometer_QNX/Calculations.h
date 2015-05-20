50d0c642f281ba8d1e52db4dd7e223421500539a
Chuck Michaels <chuck@cme.mshome.net>
2015-05-20 01:40:53 -0400

    adding a reference and setting of the WheelSensor required during calculations

--- Cyclometer_QNX/Calculations.h
+++ Cyclometer_QNX/Calculations.h
@@ -7,6 +7,7 @@
 #define CALCULATIONS_H
 
 #include "events.h"
+#include "WheelSensor.h"
 
 #define ACCUM_TIMEOUT_NS	(830000000)
 
@@ -36,11 +37,16 @@ public:
 
 	//passed the last event triggered to Calculations class
 	void eventUpdate( events lastEvent){curEvent = lastEvent;}
+
+	//attaching the externally init WheelSensor
+	void attachPulseCounter(WheelSensor* w) {pc = w;}
 private:
 	events curEvent;
 
 	double _speed;
 	double _avg;
 	double _dist;
+	//reference to a pre-init WheelSensor
+	WheelSensor* pc;	//for monitoring the pulse counts
 };
 #endif //CALCULATIONS_H
